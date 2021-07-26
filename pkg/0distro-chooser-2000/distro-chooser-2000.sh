0ff2ca8a1af86fdd4ab9e5c40b42de088ecad5a1
teleguest <tele@linux.pl>
2021-07-26 23:49:04 +0200

    .

--- pkg/0distro-chooser-2000/distro-chooser-2000.sh
+++ pkg/0distro-chooser-2000/distro-chooser-2000.sh
@@ -1,6 +1,5 @@
-wget https://openusersystem.github.io/packages/pkg/0distro-chooser-2000/main.cpp
-g++ main.cpp
-echo /home/$USER/.nicpkg/a.out > distro-chooser-2000
+wget https://openusersystem.github.io/packages/pkg/0distro-chooser-2000/dc2000.out
+echo /home/$USER/.nicpkg/dc2000.out > distro-chooser-2000
 chmod +x distro-chooser-2000
 sudo cp distro-chooser-2000 /usr/bin/
-rm distro-chooser-2000.sh distro-chooser-2000 main.cpp
+rm distro-chooser-2000.sh distro-chooser-2000
