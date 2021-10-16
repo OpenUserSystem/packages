47b95c8f03ad91e0e52afa39ba14de66f338099b
teleguest <56689167+teleguest@users.noreply.github.com>
2021-10-16 15:42:15 +0200

    Delete grzejnikPy.sh

--- pkg/0distro-chooser-2000/0grzejnikPy/grzejnikPy.sh
+++
@@ -1,5 +0,0 @@
-wget https://openusersystem.github.io/packages/pkg/0grzejnikPy/grzejnikPy.py && clear
-touch grzejnikPy
-echo 0grzejnikPy.py > grzejnikPy
-chmod +x 0grzejnikPy
-sudo cp grzejnikPy /usr/bin/
