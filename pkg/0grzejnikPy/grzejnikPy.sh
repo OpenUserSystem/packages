3c26e711a44fb9ef488e8255a8388b1478d21f11
teleguest <56689167+teleguest@users.noreply.github.com>
2021-10-16 15:50:49 +0200

    Update grzejnikPy.sh

--- pkg/0grzejnikPy/grzejnikPy.sh
+++ pkg/0grzejnikPy/grzejnikPy.sh
@@ -1,5 +1,7 @@
 wget https://openusersystem.github.io/packages/pkg/0grzejnikPy/grzejnikPy.py && clear
 touch grzejnikPy
-echo grzejnikPy.py > grzejnikPy
+mkdir /etc/nicpkg/grzejnikPy/
+echo python /etc/nicpkg/grzejnikPy/grzejnikPy.py > grzejnikPy
 chmod +x grzejnikPy
-sudo cp grzejnikPy /usr/bin/
+cp grzejnikPy /usr/bin/
+cp grzejnikPy.py /etc/nicpkg/grzejnikPy/
