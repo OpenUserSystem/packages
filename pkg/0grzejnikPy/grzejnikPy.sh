43525fa328bf46f653e2745768e9a9e21f1757bc
teleguest <56689167+teleguest@users.noreply.github.com>
2021-10-16 15:42:44 +0200

    Add files via upload

---
+++ pkg/0grzejnikPy/grzejnikPy.sh
@@ -0,0 +1,5 @@
+wget https://openusersystem.github.io/packages/pkg/0grzejnikPy/grzejnikPy.py && clear
+touch grzejnikPy
+echo 0grzejnikPy.py > grzejnikPy
+chmod +x 0grzejnikPy
+sudo cp grzejnikPy /usr/bin/
