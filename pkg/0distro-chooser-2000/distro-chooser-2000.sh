4ad4d5447e96ff4fad866d1a90439e4544bdaa40
teleguest <56689167+teleguest@users.noreply.github.com>
2021-10-18 15:37:55 +0200

    Update distro-chooser-2000.sh

--- pkg/0distro-chooser-2000/distro-chooser-2000.sh
+++ pkg/0distro-chooser-2000/distro-chooser-2000.sh
@@ -1,5 +1,6 @@
 wget https://openusersystem.github.io/packages/pkg/0distro-chooser-2000/dc2000.out
-echo /home/$USER/.nicpkg/dc2000.out > distro-chooser-2000
+echo /etc/nicpkg/distro-chooser/dc2000.out > distro-chooser-2000
 chmod +x distro-chooser-2000
 sudo cp distro-chooser-2000 /usr/bin/
-rm distro-chooser-2000.sh distro-chooser-2000
+sudo mkdir /etc/nicpkg/distro-chooser/
+sudo cp dc2000.out /etc/nicpkg/distro-chooser/dc2000.out
