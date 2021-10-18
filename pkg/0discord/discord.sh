ed1b31028a0a37f7c5dbcc294e31593373aa41bc
teleguest <56689167+teleguest@users.noreply.github.com>
2021-10-18 16:04:01 +0200

    Add files via upload

---
+++ pkg/0discord/discord.sh
@@ -0,0 +1,9 @@
+file= wget https://dl.discordapp.net/apps/linux/0.0.16/discord-0.0.16.tar.gz
+touch discord
+echo /etc/nicpkg/discord/Discord/ > discord
+chmod +x discord
+sudo mkdir /etc/nicpkg/discord/
+sudo cp discord-0.0.16.tar.gz /etc/nicpkg/discord/
+sudo cp discord /usr/bin/
+sudo tar -xf /etc/nicpkg/discord/discord-0.0.16.tar.gz
+rm /etc/nicpkg/discord/discord-0.0.16.tar.gz
