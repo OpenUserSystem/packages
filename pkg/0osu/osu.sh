bf315ef98c9e40adb188190cbebc439f692dad6d
teleguest <56689167+teleguest@users.noreply.github.com>
2021-11-28 20:49:52 +0100

    Update osu.sh

--- pkg/0osu/osu.sh
+++ pkg/0osu/osu.sh
@@ -2,6 +2,6 @@ wget https://github.com/ppy/osu/releases/latest/download/osu.AppImage && clear
 touch osu
 sudo mkdir /etc/nicpkg/osu/
 echo /etc/nicpkg/osu/osu.AppImage > osu
-chmod +x osu && chmod osu.AppImage
-sudo cp osu /etc/nicpkg/osu
-sudo cp osu..AppImage
+chmod +x osu && chmod +x osu.AppImage
+sudo cp osu /usr/bin/
+sudo cp osu.AppImage /etc/nicpkg/osu/
